480260b965d3263b1ee8d3262814f23de7f2049d
unknown <you@example.com>
2026-06-20 08:19:48 +0700

    test php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,16 @@
 <?php
 
-require __DIR__.'/../public/index.php';
+$paths = [
+    '/tmp/views',
+    '/tmp/cache',
+    '/tmp/sessions',
+    '/tmp/testing',
+];
+
+foreach ($paths as $path) {
+    if (!is_dir($path)) {
+        mkdir($path, 0777, true);
+    }
+}
+
+require __DIR__ . '/../public/index.php';
